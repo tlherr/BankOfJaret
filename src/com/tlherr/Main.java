bd5f019ea8e370f851111fd78045365e62e53fbf
Thomas Herr <tlherr@users.noreply.github.com>
2015-12-21 16:07:53 -0500

    Adding ignore file and main base classes.

---
+++ src/com/tlherr/Main.java
@@ -0,0 +1,12 @@
+package com.tlherr;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        JFrame mainForm = new MainForm();
+        mainForm.setVisible(true);
+    }
+}
